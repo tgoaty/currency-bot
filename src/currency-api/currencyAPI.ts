64f025519276099165a44dc29e79d061217b7ef6
Pushkin Nikolay <adwedelf@gmail.com>
2025-03-12 23:06:45 +0700

    ref: currencyAPI class

--- src/currency-api/currencyAPI.ts
+++ src/currency-api/currencyAPI.ts
@@ -21,13 +21,30 @@ interface JsonCBR {
 export class CurrencyAPI {
 	private static readonly url: string =
 		"https://www.cbr-xml-daily.ru/daily_json.js";
+	private static cachedData: JsonCBR | null = null;
+	private static lastFetchTimestamp: number = 0;
+	private static readonly CACHE_DURATION: number = 60 * 60 * 1000; // 1 hour
 
 	static async getJsonCBR(): Promise<JsonCBR> {
-		const response = await fetch(this.url);
-		return await response.json();
+		const now = Date.now();
+		if (
+			this.cachedData && now - this.lastFetchTimestamp <= this.CACHE_DURATION
+		) {
+			return this.cachedData;
+		}
+		try {
+			const response = await fetch(this.url);
+
+			this.cachedData = await response.json();
+			this.lastFetchTimestamp = now;
+
+			return this.cachedData as JsonCBR;
+		} catch (error) {
+			throw error;
+		}
 	}
 
-	static async getCurrencyList() {
+	static async getCurrencyList(): Promise<string[]> {
 		const json = await this.getJsonCBR();
 		return Object.keys(json.Valute);
 	}
